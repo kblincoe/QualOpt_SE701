509fa9c96228b7ddbe4482357a901d537fa1188d
Fraser <lewissmith.fraser@gmail.com>
2017-07-29 14:44:18 +1200

    Added Roles
    Created ROLE_RESEARCHER and ROLE_PARTICIPANT, these will be used for manager user types (ROLE_USER will become redundant)

--- src/main/java/org/project36/qualopt/security/AuthoritiesConstants.java
+++ src/main/java/org/project36/qualopt/security/AuthoritiesConstants.java
@@ -9,6 +9,10 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String RESEARCHER = "ROLE_RESEARCHER";
+
+    public static final String PARTICIPANT = "ROLE_PARTICIPANT";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {
