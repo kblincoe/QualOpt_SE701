372452955908dfda536a9773b138e23e53e7963f
umih874 <umih874@aucklanduni.ac.nz>
2018-03-07 21:21:42 +1300

    Added current password checker on the password reset page.

--- src/main/webapp/app/account/password/password.component.ts
+++ src/main/webapp/app/account/password/password.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Principal } from '../../shared';
 import { PasswordService } from './password.service';
-
+import { Account } from '../../shared';
+import { LoginService } from "../../shared/login/login.service"; 
 @Component({
     selector: 'jhi-password',
     templateUrl: './password.component.html'
@@ -11,11 +12,13 @@ export class PasswordComponent implements OnInit {
     doNotMatch: string;
     error: string;
     success: string;
-    account: any;
+    account: Account;
     password: string;
+    currentPassword: string; 
     confirmPassword: string;
-
+    wrongPassword  : boolean; 
     constructor(
+        private loginService: LoginService,
         private passwordService: PasswordService,
         private principal: Principal
     ) {
@@ -28,19 +31,36 @@ export class PasswordComponent implements OnInit {
     }
 
     changePassword() {
-        if (this.password !== this.confirmPassword) {
-            this.error = null;
-            this.success = null;
-            this.doNotMatch = 'ERROR';
-        } else {
-            this.doNotMatch = null;
-            this.passwordService.save(this.password).subscribe(() => {
+        // Check if we can vaidate the entered password.
+        this.loginService.login({
+            username: this.account.login,
+            password: this.currentPassword,
+            rememberMe: this.loginService.getRememberMe()
+        }).then(() => {
+            // check if new passwords match 
+            if (this.password !== this.confirmPassword) {
                 this.error = null;
-                this.success = 'OK';
-            }, () => {
                 this.success = null;
-                this.error = 'ERROR';
-            });
-        }
+                this.doNotMatch = 'ERROR';
+            } 
+            //update password
+            else{
+                this.wrongPassword = false;
+                this.doNotMatch = null;
+                this.passwordService.save(this.password).subscribe(() => {
+                    this.error = null;
+                    this.success = 'OK';
+                }, () => {
+                    this.success = null;
+                    this.error = 'ERROR';
+                });
+            }
+        // if password is invalid then throw error message
+        }).catch(() => {
+            this.success = null;
+            this.wrongPassword = true; 
+        });
+
     }
+
 }
