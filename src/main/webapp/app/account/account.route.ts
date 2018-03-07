da72d44190db46edc4e320197631b70b6da15f27
Fraser McIntosh <frasermcintosh1@gmail.com>
2018-03-07 15:45:40 +1300

    Remove sessions page and associated services

--- src/main/webapp/app/account/account.route.ts
+++ src/main/webapp/app/account/account.route.ts
@@ -6,7 +6,6 @@ import {
     passwordResetFinishRoute,
     passwordResetInitRoute,
     registerRoute,
-    sessionsRoute,
     socialRegisterRoute,
     settingsRoute
 } from './';
@@ -17,7 +16,6 @@ const ACCOUNT_ROUTES = [
     passwordResetFinishRoute,
     passwordResetInitRoute,
     registerRoute,
-    sessionsRoute,
     socialRegisterRoute,
     settingsRoute
 ];
