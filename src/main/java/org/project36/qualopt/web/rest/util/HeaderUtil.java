682a8d4cae3b4b2e141c85f62a0402b5cb66e248
Hanliang Ding <hdin898@aucklanduni.ac.nz>
2018-03-18 20:33:26 +1300

    Change popup "identifier" to "name" for studies

--- src/main/java/org/project36/qualopt/web/rest/util/HeaderUtil.java
+++ src/main/java/org/project36/qualopt/web/rest/util/HeaderUtil.java
@@ -21,15 +21,15 @@ public final class HeaderUtil {
     }
 
     public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
-        return createAlert("A new " + entityName + " is created with identifier " + param, param);
+        return createAlert("A new " + entityName + " is created with name " + param, param);
     }
 
     public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
-        return createAlert("A " + entityName + " is updated with identifier " + param, param);
+        return createAlert("A " + entityName + " is updated with name " + param, param);
     }
 
     public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
-        return createAlert("A " + entityName + " is deleted with identifier " + param, param);
+        return createAlert("A " + entityName + " is deleted with name " + param, param);
     }
 
     public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
